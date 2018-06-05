b7d397cb23d3ac6adcd9a1e79f1983642e33471e
brizism <montielbriseida@gmail.com>
2018-06-04 21:08:14 -0400

    event on message

--- index.js
+++ index.js
@@ -7,3 +7,35 @@ const bot = new SlackBot({
 })
 
 
+// Start Handler
+bot.on('start', () => {
+  const params = {
+    icon_emoji: ':smiley:'
+  }
+
+  bot.postMessageToChannel('general', 'Im birding baby @brizism', params);
+})
+
+// Error Handler
+bot.on('error', (err) => console.log(err));
+
+// Message Handler
+bot.on('message', (data) => {
+  if(data.type !== 'message'){
+    return;
+  }
+
+  handleMessage(data.text);
+})
+
+// Responds to Data
+function handleMessage(message){
+  if(message.includes(' chucknorris')){
+    chuckJoke();
+  }
+}
+
+// Tell a Chuck Norris Joke
+function chuckJoke(){
+  
+}
