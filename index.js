cf0c1bbb7effc218a3152f659a636cdf1c21bc51
brizism <montielbriseida@gmail.com>
2018-06-04 20:40:40 -0400

    init bot

---
+++ index.js
@@ -0,0 +1,9 @@
+const SlackBot = require('slackbots');
+const axios = require('axios');
+
+const bot = new SlackBot({
+  token: 'xoxb-375058847089-375380001508-c5ewrfzpAd2kZy6EpW6XNOMj',
+  name: 'duckbot'
+})
+
+
